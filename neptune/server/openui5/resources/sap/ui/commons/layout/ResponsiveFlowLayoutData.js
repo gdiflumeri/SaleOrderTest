d323e61bdd1fe22ac11ac241497d500e122f1bf0
gdiflumeri <gianmaria.diflumeri@colsam.com>
2015-02-25 10:35:52 +0100

    Cancellazione

--- neptune/server/openui5/resources/sap/ui/commons/layout/ResponsiveFlowLayoutData.js
+++
@@ -1,9 +0,0 @@
-/*!
- * SAP UI development toolkit for HTML5 (SAPUI5)
- * 
- * (c) Copyright 2009-2014 SAP SE. All rights reserved
- */
-jQuery.sap.declare("sap.ui.commons.layout.ResponsiveFlowLayoutData");jQuery.sap.require("sap.ui.commons.library");jQuery.sap.require("sap.ui.layout.ResponsiveFlowLayoutData");sap.ui.layout.ResponsiveFlowLayoutData.extend("sap.ui.commons.layout.ResponsiveFlowLayoutData",{metadata:{deprecated:true,library:"sap.ui.commons"}});
-/*!
- * @copyright@
- */
